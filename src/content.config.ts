18ce82fb3a92582bddad2853a706b64fbed6c511
sujansingh9749-collab <Sujansingh9749@gmail.com>
2026-06-07 21:54:37 +0530

    First time push - Fresh Astro site

---
+++ src/content.config.ts
@@ -0,0 +1,15 @@
+import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
+
+const blog = defineCollection({
+  loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.coerce.date(),
+    youtubeId: z.string().optional(),
+    author: z.string().default('ByteForge'),
+  }),
+});
+
+export const collections = { blog };
